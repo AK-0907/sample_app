215c56d3e8ef16092c7738e1d0dd87ded5d719ea
ax0907 <aloha.aoto0907@gmail.com>
2023-12-10 20:03:31 +0900

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,11 @@ class UsersController < ApplicationController
     end
   end  
 
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end  
+
   private
 
   def user_params
